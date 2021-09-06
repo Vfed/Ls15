165ef38636e111cd31489a79579d6acc32bc562e
Vfed <asser.282195@gmail.com>
2021-09-06 19:40:57 +0300

    Add project files.

---
+++ Group.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ls15
+{
+    public enum position { solo, back, gitara }
+    public class Group : IGroup
+    {
+        public string GropName { get; set; }
+        public IBand Band { get; set; }
+        public List<IAlbume> Albumes { get; set; }
+    }
+
+    public class Person : IPerson
+    {
+        public int Name { get; set; }
+        public position Position { get; set; }
+    }
+    public class Band : IBand
+    {
+        public List<IPerson> MyBand { get; set; }
+        public int Foundation { get; set; }
+    }
+    public class Song : ISong
+    {
+        public string SongName { get; set; }
+    }
+    public class Albume : IAlbume
+    {
+        public List<ISong> Songs { get; set; }
+        public int AlbumeYear { get; set; }
+        public string AlbumeGenre { get; set; }
+        public string AlbumeName { get; set; }
+    }
+}
---
+++ IGroup.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ls15
+{
+    
+    public interface IGroup 
+    {
+        public string GropName { get; set; }
+        public IBand Band { get; set; }
+        public List<IAlbume> Albumes { get; set; }
+    }
+
+    public interface IPerson 
+    {
+        public int Name { get; set; }
+        public position Position { get; set; }
+    }
+    public interface IBand
+    {
+        public List<IPerson> MyBand { get; set; }
+        public int Foundation { get; set; }
+    }
+    public interface ISong 
+    {
+        public string SongName { get; set; }
+    }
+    public interface IAlbume
+    {
+        public List<ISong> Songs { get; set; }
+        public int AlbumeYear { get; set; }
+        public string AlbumeGenre { get; set; }
+        public string AlbumeName { get; set; }
+    }
+}
